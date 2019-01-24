98f6018fc830d29bce4ff49ea7046086ef724140
BandurkoAleksandr <AleksandBandurko@gmail.com>
2019-01-24 15:20:16 +0200

    add buttons for tasks

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,18 @@
 // добавить закладку(кнопку) для каждой задачи
+let taskNumber = document.querySelectorAll('.task');
+let tasks = document.querySelector('.tasks');
+let kingTask = document.querySelector('.kingTask');
+let backButton = document.querySelector('.back');
 
+taskNumber[0].addEventListener('click', function() {
+    kingTask.style.display = 'block';
+    backButton.style.display = 'block';
+    tasks.style.display = 'none';
+});
+
+
+backButton.addEventListener('click', function() {
+    kingTask.style.display = 'none';
+    backButton.style.display = 'none';
+    tasks.style.display = 'block';
+})
