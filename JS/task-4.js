c4e4d23e7224a061d4e75398f75a6270f89efb44
BandurkoAleksandr <AleksandBandurko@gmail.com>
2019-02-08 15:51:42 +0200

    some fixes in 4-th task(not all)!

--- JS/task-4.js
+++ JS/task-4.js
@@ -4,10 +4,11 @@ function initLuckyNumber () {
     let finishNumberInput = document.querySelector('.finish-number-input');
     let startNumberButton = document.querySelector('.start-number-button');
     let warningBox = document.querySelector('.warning-4');
-// fn (findNumbersQuantity) takes two arguments: minimum and maximum N-digit numbers
+// fn (findNumbersQuantity) takes two arguments(strings): minimum and maximum N-digit numbers
 // each number is separated into two equal parts, 
 // then nested fn (findSumOfHalf) takes first or second parts of both numbers (two arguments)
-// nested fn creates an array whose length is equal to the maximum sum of numbers N/2
+// and third optional argument (arr).
+// Nested fn creates an array whose length is equal to the maximum sum of numbers N/2
 // and assigns each cell a value of 0 (first cycle)
 // the second cycle goes through all the numbers in the range 
 // from minimum to maximum (passed arguments),
@@ -20,18 +21,17 @@ function initLuckyNumber () {
 // then in the cycle multiply the results of call
 // and the result of each iteration is summarized with the previous one.(RESULT!!!)
     function findNumbersQuantity (min, max) {
-        let halfDigitLength = max.toString().length / 2;
-        let minString = min.toString();
-
-        let minLeft = minString.slice(0, halfDigitLength);
-        let minRight = minString.slice(halfDigitLength);
-        let maxLeft = max.toString().slice(0, halfDigitLength);
-        let maxRight = max.toString().slice(halfDigitLength);
+        let halfDigitLength = max.length / 2;
+        let minLeft = +min.slice(0, halfDigitLength);
+        let minRight = +min.slice(halfDigitLength);
+        let maxLeft = +max.slice(0, halfDigitLength);
+        let maxRight = +max.slice(halfDigitLength);
         let result = 0;
+        console.log(typeof(minLeft));
         
         function findSumOfHalf (minHalf, maxHalf, arr) {
             let arrLength = 9 * maxHalf.toString().length;
-            console.log(minHalf, maxHalf, arr);
+            // console.log(minHalf, maxHalf, arr);
             arr = arr || [];
             for (let i = arr.length; i < arrLength; i++) {
                 arr[i] = 0;
@@ -49,17 +49,17 @@ function initLuckyNumber () {
         // exsample: 100 - 999  
         let leftSum = findSumOfHalf(minLeft, maxLeft);
         let rightSum = leftSum.slice(0);
-        // exsample: 000 - (100 - 1); 
-        findSumOfHalf(minRight, minLeft - 1, rightSum);  
-        
+        // exsample: 000 - (100 - 1), 100 - 999; 
+        findSumOfHalf(minRight, minLeft - 1, rightSum);
+  
         for (let i = 0; i < leftSum.length; i++) {
             result += leftSum[i] * rightSum[i];
         } 
         warningBox.innerHTML = result;
     }
     startNumberButton.addEventListener('click', function () {
-        let startNumber = +startNumberInput.value;
-        let finishNumber = +finishNumberInput.value;
+        let startNumber = startNumberInput.value;
+        let finishNumber = finishNumberInput.value;
         
         if (startNumber == NaN || startNumber < 0 || startNumberInput.value == '') {
             startNumberInput.value = startNumberInput.placeholder;
