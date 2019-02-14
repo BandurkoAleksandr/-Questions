31dd4aaa7a7ec423c2b6cc313156ef15181a2d19
BandurkoAleksandr <AleksandBandurko@gmail.com>
2019-02-14 22:07:27 +0200

    rewrote task 9 version 0.2

--- JS/task-9.js
+++ JS/task-9.js
@@ -3,65 +3,49 @@ function initIsPeriodicSequence () {
     let warningBox = document.querySelector('.warning-9');
     let isPeriodicSequenceInput = document.querySelector('.is-periodic-sequence');
     let isPeriodicSequenceButton = document.querySelector('.button-for-periodic-sequence');
-// передаем в метод sort()
-    function compareNumeric(a, b) {
-        return a - b;
-    }
-// получаем количество слов в стороке
-    function numberOfWords (str) {
-        let arr = str.split('').sort();
-        let obj = {};
-        let result = [];
 
-        for (let i = 0; i < arr.length; i++) {
-            let key = arr[i];
-            if (!obj[key]) {
-                obj[key] = 0;
-            }
-            obj[key] += 1;
-        }
-        for (let key in obj) {
-            result.push(obj[key]);
-        }
-        return result = result.sort(compareNumeric)[0];
-    }
-// Определяем, является ли периодической последовательностью строка символов
-    function isPeriodicSequence (str, fn) {
-        let quantity = fn(str);
-        let arr = str.split('');
-        let wordLength = arr.length / quantity;
-        console.log(quantity);
-        console.log(arr.length);
-        console.log(wordLength);
-        let arrOfWords = [];
+
+    function isPeriodicSequence (str) {
         let result = '';
-        // отсеиваем варианты, кроме строки состоящей из разных анаграмм
-        if (quantity < 2 || wordLength % 1 != 0) {
+        let currentSequence = '';
+        let wordLength;
+        // ищем длину слова и производим первые проверки
+        // как только нашли длину слова, выходим из цикла
+        for (let i = 0; i < str.length; i++) {
+            currentSequence += str[i];
+            let index = str.indexOf(currentSequence, i + 1);
+
+            if (index == -1) {
+                return result = 'non-periodic sequence';
+            } else if (index == currentSequence.length) {
+                wordLength = index;
+                break;
+            }
+        }
+        // зная длину слова, проверяем состоит ли строка из слов одинаковой длины
+        if (str.length % wordLength != 0) {
             return result = 'non-periodic sequence';
         }
-        // получаем массив слов из которых состоит строка(возможны анаграммы)
-        for (let i = 0; i < quantity; i++) {
-            arrOfWords.push(arr.splice(0, wordLength).join(''));
-        }
-        console.log(arrOfWords);
-        // сравниваем слова друг с другом
-        for (let j = 0; j < arrOfWords.length - 1; j++) {
-            let x = arrOfWords[j].localeCompare(arrOfWords[j + 1]);
-            if (x != 0) {
-                 return result = 'non-periodic sequence';
-            } else {
-                result = 'periodic sequence of words: ' + arrOfWords[0];
+        // получаем слово из которого должна состоять последовательность
+        let word = str.substring(0, wordLength); 
+        // получаем массив состоящих из пустых ячеек и инородных слов
+        let arr = str.split(word);
+        console.log(word);
+        console.log(arr);
+        // проверяем на инородные слова и анаграммы
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j].length != 0) {
+                return result = 'non-periodic sequence';
             }
-            
         }
-        return result;
+        return result = 'periodic sequence of words: ' + word;
     }
 
 // нет проверок на ввод строки
     isPeriodicSequenceButton.addEventListener('click', function () {
         let stringOfWords = isPeriodicSequenceInput.value;
         isPeriodicSequenceInput.value = '';
-        let fnResult = isPeriodicSequence(stringOfWords, numberOfWords);
+        let fnResult = isPeriodicSequence(stringOfWords);
         warningBox.innerHTML = '<p>Incoming data: ' + stringOfWords + '.</p>' + '<p>Result: ' + fnResult + '.</p>';
     });
 
