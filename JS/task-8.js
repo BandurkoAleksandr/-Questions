7bb29f1904cff67ff37a96ea3af1e1488b79268e
BandurkoAleksandr <AleksandBandurko@gmail.com>
2019-02-18 14:05:23 +0200

    rewrote task 8 removed recursion, use indexOf.

--- JS/task-8.js
+++ JS/task-8.js
@@ -4,24 +4,17 @@ function initDeletionOfNumbers () {
     let aNumberInput = document.querySelector('.A-number-input');
     let bNumberInput = document.querySelector('.B-number-input');
     let numbersButton = document.querySelector('.numbers-button');
-    // function calls itself until the a argument goes black
+
     function deletionOfNumbers (a, b) {
+        let indexB = 0;
         let result = '';
-        let j;
-        
         for (let i = 0; i < a.length; i++) {
-            // console.log("f", a, b);
-            for (j = 0; j < b.length; j++) {
-                if (b.charAt(j) == a[i]) {
-                    b = b.slice(j + 1);
-                    a.splice(i, 1);
-                    // console.log(a, a.length, b);
-                    deletionOfNumbers(a, b);
-                }
+            let index = b.indexOf(a.charAt(i), indexB);
+            if (index > -1) {
+                indexB = index;
+            } else {
+                return result = 'NO';
             }
-        }
-        if (a.length != 0) {
-            return result = 'NO';
         }
         return result = 'Yes';  
     }
@@ -29,8 +22,8 @@ function initDeletionOfNumbers () {
     // then if all is good, call the deletionOfNumbers function
     // with numbers A, B as arguments
     numbersButton.addEventListener('click', function () {
-        // typeof = string
-        let aNumber = aNumberInput.value.split('');
+        
+        let aNumber = aNumberInput.value;
         let bNumber = bNumberInput.value;
         
         if (aNumber == NaN || aNumber < 0 || aNumberInput.value == '' || aNumber.indexOf('.') > 0) {
@@ -48,3 +41,68 @@ function initDeletionOfNumbers () {
     });
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function initDeletionOfNumbers () {
+//     // Task q20
+//         let warningBox = document.querySelector('.warning-8');
+//         let aNumberInput = document.querySelector('.A-number-input');
+//         let bNumberInput = document.querySelector('.B-number-input');
+//         let numbersButton = document.querySelector('.numbers-button');
+    
+//         function deletionOfNumbers (a, b) {
+//             let result = '';
+//             let j;
+            
+//             for (let i = 0; i < a.length; i++) {
+//                 console.log("f", a, b);
+//                 for (j = 0; j < b.length; j++) {
+//                     if (b[j] == a[i]) {
+//                         b.splice(0, j + 1);
+//                         a.splice(i, 1);
+//                         console.log(a, a.length, b);
+//                         deletionOfNumbers(a, b);
+//                     }
+//                 }
+//             }
+//             if (a.length != 0) {
+//                 return result = 'NO';
+//             }
+//             return result = 'Yes';  
+//         }
+//         // get A and B numbers
+//         // then if all is good, call the deletionOfNumbers function
+//         // with numbers A, B as arguments
+//         numbersButton.addEventListener('click', function () {
+            
+//             let aNumber = aNumberInput.value.split('');
+//             let bNumber = bNumberInput.value.split('');
+            
+//             if (aNumber == NaN || aNumber < 0 || aNumberInput.value == '' || aNumber.indexOf('.') > 0) {
+//                 aNumberInput.value = aNumberInput.placeholder;
+//                 return alert('Enter an integer >= 0');
+//             } else if  (bNumber.length <= aNumber.length || bNumber.indexOf('.') > 0) {
+//                 bNumberInput.value = bNumberInput.placeholder;
+//                 return alert('B number have to be an integer and longest than start number');
+//             } else if (bNumber.length > aNumber.length) {
+//                 aNumberInput.value = aNumberInput.placeholder;
+//                 bNumberInput.value = bNumberInput.placeholder;
+//                 let fnResult = deletionOfNumbers(aNumber, bNumber);
+//                 warningBox.innerHTML = '<p>Result: ' + fnResult + '.</p>';
+//             }
+//         });
+    
+//     }
